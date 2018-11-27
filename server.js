8b243ad7b2c490acd59aeac361354ba787d443ea
Prasanna Sriram <pras306@gmail.com>
2018-11-28 03:46:42 +0530

    adding port details

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.put('/images', (req,res) => image.handleImage(req, res, db));
 
 app.post('/imageurl', (req,res) => image.handleApiCall(req, res));
 
-app.listen(3000, () => {
-	console.log('app is running');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on ${process.env.PORT}`);
 });
 
 
